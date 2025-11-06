015399e7eba7ce07b03902530da3507547cf5ec4
Raushan-Singh <raushan786267@gmail.com>
2025-11-06 12:22:59 +0530

    sorting

---
+++ _05_Recursion/sorting/_03_InversionCount.java
@@ -0,0 +1,6 @@
+
+public class _03_InversionCount {
+  public static void main(String[] args) {
+
+  }
+}
---
+++ _05_Recursion/sorting/_04_QuickSort.java
@@ -0,0 +1,59 @@
+public class _04_QuickSort {
+
+  public static void swap(int[] arr, int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+
+  public static void print(int arr[]) {
+    for (int ele : arr) {
+      System.out.print(ele + " ");
+    }
+    System.out.println();
+  }
+
+  public static int partition(int arr[], int lo, int hi) {
+    int pivot = arr[lo];
+    int pivotIdx = lo;
+    int smallerCount = 0;
+    for (int i = lo + 1; i <= hi; i++) {
+      if (arr[i] <= pivot)
+        smallerCount++;
+    }
+    int correctIdx = pivotIdx + smallerCount;
+    // swap arr[pidx] and arr[correctIdx]
+    swap(arr, pivotIdx, correctIdx);
+    // partition
+    int i = lo;
+    int j = hi;
+    while (i < correctIdx && j > correctIdx) {
+      if (arr[i] <= pivot) {
+        i++;
+      } else if (arr[j] > pivot) {
+        j--;
+      } else if (arr[i] > pivot && arr[j] <= pivot) {
+        swap(arr, i, j);
+      }
+    }
+    return correctIdx;
+  }
+
+  public static void quickSort(int[] arr, int lo, int hi) {
+    // pivot (arr[0]) ko sahi jagah rakho
+    // & left part me <=pivot
+    if (lo >= hi)
+      return;
+    int idx = partition(arr, lo, hi);
+    quickSort(arr, lo, idx - 1);
+    quickSort(arr, idx + 1, hi);
+  }
+
+  public static void main(String[] args) {
+    int arr[] = { 4, 9, 7, 1, 2, 3, 6, 5, 8 };
+    int n = arr.length;
+    print(arr);
+    quickSort(arr, 0, n - 1);
+    print(arr);
+  }
+}
