6f1998c2259f98eb1b0c89e21b6bddc357f7635b
Raushan Singh <raushan786267@gmail.com>
2026-02-14 23:32:39 +0530

    1D dp practice

---
+++ _15_DP/_02_ClimbingStair.java
@@ -0,0 +1,26 @@
+
+class Solution {
+   
+    int count(int n,int[] dp){
+        if(n==0||n==1)return 1;
+        if(dp[n]!=-1)return dp[n];
+        int oneStep=count(n-1,dp);
+        int twoStep=count(n-2,dp);
+        return dp[n]=oneStep+twoStep;
+    }
+    public int climbStairs(int n) {
+       
+    int dp[]=new int[n+1];
+    Arrays.fill(dp,-1);
+       return count(n,dp);
+    }
+}
+
+
+
+
+
+
+public class _02_ClimbingStair {
+  
+}
