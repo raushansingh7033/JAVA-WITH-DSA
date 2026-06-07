23125bf230a874443dd3abc9d8854af797edb6e6
raushansingh7033 <raushan786267@gmail.com>
2026-06-07 10:40:13 +0530

    string pratice

--- _04_String/_16_PalindromicSubString.java
+++ _04_String/_16_PalindromicSubString.java
@@ -15,7 +15,7 @@ public class _16_PalindromicSubString {
   public static void main(String[] args) {
     String str = "abbd";
     int count = 0;
-    for (int i = 0; i < str.length() - 1; i++) {
+    for (int i = 0; i < str.length() ; i++) {
       for (int j = i + 1; j < str.length() + 1; j++) {
         if (isPalindrome(str.substring(i, j)) == true) {
           System.out.print(str.substring(i, j) + " ");
