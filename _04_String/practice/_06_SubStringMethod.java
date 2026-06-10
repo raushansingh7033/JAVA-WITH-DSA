ba464c226b32cf2997423b1122f56f3a39ae80aa
raushansingh7033 <raushan786267@gmail.com>
2026-06-10 22:09:46 +0530

    pratice of String

---
+++ _04_String/practice/_06_SubStringMethod.java
@@ -0,0 +1,20 @@
+public class _06_SubStringMethod {
+    public static void main(String[] args) {
+     String str = "abcde";
+    System.out.println(str.substring(1));
+    System.out.println(str.substring(3));
+    System.out.println(str.substring(1, 4));
+    // print all substring
+    for (int i = 1; i <= 5; i++) {
+      System.out.println(str.substring(0, i));
+    }
+
+    for (int i = 0; i < str.length(); i++) {
+      for (int j = i + 1; j <= 5; j++) {
+        System.out.println(str.substring(i, j));
+      }
+    }
+    System.out.println("hello" + 10 + 20);
+    System.out.println(10 + 20 + "hello");
+  }
+}
---
+++ _04_String/practice/_08_ConvertingIntegerString.java
@@ -0,0 +1,7 @@
+public class _08_ConvertingIntegerString {
+ public static void main(String[] args) {
+    int a = 10;
+    String str = a + "";
+    System.out.println(str);
+  }
+}
